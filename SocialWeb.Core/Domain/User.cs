a0b118b133b1d759899e1bfe9e1bd75bd5ccd985
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-20 19:57:30 +0200

    Add User.cs
    Create basic version of User.cs

---
+++ SocialWeb.Core/Domain/User.cs
@@ -0,0 +1,90 @@
+using System;
+
+namespace SocialWeb.Core.Domain
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string Email { get; protected set; }
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public string Role { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+
+        public User(string email, string firstName, string LastName,
+                    string password, string salt, string role)
+        {
+            Id = Guid.NewGuid();
+            SetEmail(email);
+            SetFirstName(firstName);
+            SetLastName(LastName);
+            SetPassword(password, salt);
+            Role = role;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        protected User() {}
+
+        public void SetEmail(string email)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("Email is invalid");
+            }
+
+            Email = email.ToLowerInvariant();
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            if(string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new Exception("FirstName is invalid");
+            }
+
+            FirstName = firstName;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void SetLastName(string lastName)
+        {
+            if(string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new Exception("LastName is invalid");
+            }
+
+            LastName = lastName;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetPassword(string password, string salt)
+        {
+            if(string.IsNullOrEmpty(password))
+            {
+                throw new Exception("Password can not be empty");
+            }
+
+            if(string.IsNullOrEmpty(salt))
+            {
+                throw new Exception("Salt can not be empty");
+            }
+
+            if(password.Length < 8)
+            {
+                throw new Exception("Password is to short")
+            }
+
+            if(password.Length > 50)
+            {
+                throw new Exception("Password is to long");
+            }
+
+            Password = password;
+            Salt = salt;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
