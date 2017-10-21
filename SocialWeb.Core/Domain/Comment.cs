f4cdc9de99c3ba1593f9a07a6b7ab963bdbecf49
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-21 17:44:15 +0200

    Add realtion with User.cs

--- SocialWeb.Core/Domain/Comment.cs
+++ SocialWeb.Core/Domain/Comment.cs
@@ -8,14 +8,18 @@ namespace SocialWeb.Core.Domain
         public string Content { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
         public DateTime UpdatedAt { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public User User { get; protected set; }
         public Guid PostId { get; protected set; }
         public Post Post { get; protected set; }
 
-        public Comment(string content, Post post)
+        public Comment(string content, User user, Post post)
         {
             SetContent(content);
             CreatedAt = DateTime.UtcNow;
 
+            UserId = user.Id;
+            User = user;
             PostId = post.Id;
             Post = post;
         }
