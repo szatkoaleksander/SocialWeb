69f47bc84cd8fc3a6f6fb1cb14dbdd668e40a548
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-21 18:26:20 +0200

    add post and user repo implementation

---
+++ SocialWeb.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialWeb.Core.Domain;
+using SocialWeb.Core.Repositories;
+
+namespace SocialWeb.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private static ISet<Post> _post = new HashSet<Post>();
+
+        public async Task<Post> GetAsync(Guid id)
+            => await Task.FromResult(_post.SingleOrDefault(x => x.Id == id));
+
+        public async Task AddAsync(Post post)
+        {
+            _post.Add(post);
+            await Task.CompletedTask;
+        }
+        
+        public Task UpdateAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Post post)
+        {
+            _post.Remove(post);
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ SocialWeb.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialWeb.Core.Domain;
+using SocialWeb.Core.Repositories;
+
+namespace SocialWeb.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private static ISet<User> _user = new HashSet<User>();
+
+        public async Task<User> GetAsync(Guid id)
+            => await Task.FromResult(_user.SingleOrDefault(x => x.Id == id));
+
+        public async Task<User> GetAsync(string email)
+            => await Task.FromResult(_user.SingleOrDefault(x => x.Email == email));
+
+        public async Task AddAsync(User user)
+        {
+           _user.Add(user);
+           await Task.CompletedTask;
+        }
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _user.Remove(user);
+            await Task.CompletedTask;
+        }
+    }
+}
