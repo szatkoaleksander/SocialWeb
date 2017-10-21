46840bd41e25c2158a39a3611ca4a03d55d8cfcc
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-21 17:42:12 +0200

    Create Post.cs
    create class with properties, validation and relations with User.cs and Comment.cs

---
+++ SocialWeb.Core/Domain/Post.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace SocialWeb.Core.Domain
+{
+    public class Post
+    {
+        public Guid Id { get; protected set; }
+        public string Content { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public User User { get; protected set; }
+        public ICollection<Comment> Comments { get; protected set; }
+
+        public Post(string content, User user)
+        {
+            SetContent(content);
+            CreatedAt = DateTime.UtcNow;
+
+            UserId = user.Id;
+            User = user;
+            Comments = new List<Comment>();
+        }
+
+        protected Post() {}
+
+        public void SetContent(string content)
+        {
+            if(string.IsNullOrEmpty(content))
+            {
+                throw new Exception("Content can not be empty");
+            }
+
+            if(content.Length > 50000)
+            {
+                throw new Exception("Content is to long");
+            }
+            
+            Content = content;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
