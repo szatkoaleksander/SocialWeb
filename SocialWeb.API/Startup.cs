153824bfc72af1f5152dca79cc4fc7b2a082892b
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-22 12:49:46 +0200

    configure autofac and add automapper

--- SocialWeb.API/Startup.cs
+++ SocialWeb.API/Startup.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using SocialWeb.Infrastructure.Mapper;
+using SocialWeb.Infrastructure.Repositories;
+using SocialWeb.Infrastructure.IoC.Modules;
 
 namespace SocialWeb.API
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -22,22 +31,32 @@ namespace SocialWeb.API
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.AddSingleton(AutoMapperConfig.Initialize());
+
+            services.AddMvc()
+                .AddJsonOptions(x => x.SerializerSettings.Formatting = Formatting.Indented)
+                .AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.RegisterModule<RepositoryModule>();
+            builder.RegisterModule<ServiceModule>();
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            IApplicationLifetime appLifetime)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            appLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
         }
     }
 }
