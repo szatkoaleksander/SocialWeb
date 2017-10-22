afd730b85790047f2b55d8d0012363e785039894
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-22 13:05:58 +0200

    implement basic userDto

--- SocialWeb.Infrastructure/DTO/UserDto.cs
+++ SocialWeb.Infrastructure/DTO/UserDto.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace SocialWeb.Infrastructure.DTO
 {
     public class UserDto
     {
-        
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; protected set; }
     }
 }
