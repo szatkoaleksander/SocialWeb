f522c0a9ee6c8c5ecfa14ce20460ea9db00b27cb
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-22 12:48:00 +0200

    create automapperconfig
    for user and post

---
+++ SocialWeb.Infrastructure/Mapper/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using SocialWeb.Core.Domain;
+using SocialWeb.Infrastructure.DTO;
+
+namespace SocialWeb.Infrastructure.Mapper
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+        {
+            var config =  new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDto>();
+                cfg.CreateMap<Post, PostDto>();
+            })
+            .CreateMapper();
+
+            return config;
+        }
+    }
+}
