cef404a73ac787fdbcd3eb85f24a8892330a8901
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-22 12:50:40 +0200

    ioc

---
+++ SocialWeb.Infrastructure/IoC/Modules/ServiceModule.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using Autofac;
+using SocialWeb.Infrastructure.Services;
+
+namespace SocialWeb.Infrastructure.IoC.Modules
+{
+    public class ServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assembly = typeof(ServiceModule)
+                .GetTypeInfo()
+                .Assembly;
+
+            builder.RegisterAssemblyTypes(assembly)
+                   .Where(x => x.IsAssignableTo<IService>())
+                   .AsImplementedInterfaces()
+                   .InstancePerLifetimeScope();
+        }
+    }
+}
