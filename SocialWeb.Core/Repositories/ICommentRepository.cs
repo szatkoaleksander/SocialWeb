2e8feeb82134bdcb7a06852d3c7464b89ada22a1
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-23 22:26:49 +0200

    remove comment section

--- SocialWeb.Core/Repositories/ICommentRepository.cs
+++ SocialWeb.Core/Repositories/ICommentRepository.cs
@@ -6,8 +6,6 @@ namespace SocialWeb.Core.Repositories
 {
     public interface ICommentRepository : IRepository
     {
-         //Task<Comment> GetUserCommentsAsync(Guid id);
-         //Task<Comment> GetPostCommentsAsync(Guid id);
          Task AddAsync(Comment comment);
          Task UpdateAsync(Comment comment);
          Task RemoveAsync(Comment comment);
