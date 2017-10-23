c83a7e4af97376f37c2b075f702901585ea5e129
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-23 20:01:29 +0200

    user handler

---
+++ SocialWeb.Infrastructure/Handlers/User/CreateUserHandler.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using SocialWeb.Infrastructure.Commands.User;
+using SocialWeb.Infrastructure.Services;
+
+namespace SocialWeb.Infrastructure.Handlers.User
+{
+    public class CreateUserHandler : ICommandHandler<CreateUser>
+    {
+        private readonly IUserService _userService;
+
+        public CreateUserHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task HandlerAsync(CreateUser command)
+        {
+            await _userService.RegisterAsync(command.Email, command.FirstName, command.LastName, command.Password);
+        }
+    }
+}
