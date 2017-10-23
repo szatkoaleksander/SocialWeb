a8682e2f80c84923275b4a8e504059c5f3ba63c5
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-23 22:35:12 +0200

    create post handler

---
+++ SocialWeb.Infrastructure/Handlers/Post/CreatePostHandler.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using SocialWeb.Infrastructure.Commands.Post;
+using SocialWeb.Infrastructure.Commands.User;
+using SocialWeb.Infrastructure.Services;
+
+namespace SocialWeb.Infrastructure.Handlers.Post
+{
+    public class CreatePostHandler : ICommandHandler<CreatePost>
+    {
+        private readonly IPostService _postService;
+
+        public CreatePostHandler(IPostService postService)
+        {
+            _postService = postService;
+        }
+        public async Task HandlerAsync(CreatePost command)
+        {
+            await _postService.AddAsync(command.Content, command.UserId);
+        }
+    }
+}
