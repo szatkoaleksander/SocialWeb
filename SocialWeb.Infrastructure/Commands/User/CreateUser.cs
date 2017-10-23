577424b411cb4e5d3290100a9be2efcd7724cdd0
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-23 20:01:38 +0200

    create user command

---
+++ SocialWeb.Infrastructure/Commands/User/CreateUser.cs
@@ -0,0 +1,10 @@
+namespace SocialWeb.Infrastructure.Commands.User
+{
+    public class CreateUser : ICommand
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
