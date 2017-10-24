590b06504e46830c8890e48ded99b9b5a57b919d
szatkoaleksander <szatko.aleksander@gmail.com>
2017-10-24 14:37:22 +0200

    create login command

---
+++ SocialWeb.Infrastructure/Commands/User/Login.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SocialWeb.Infrastructure.Commands.User
+{
+    public class Login : ICommand
+    {
+        public Guid TokenId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
