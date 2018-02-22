d6fa845283e6fd5e6307b1c630fa71636976ed99
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-02-22 19:16:43 +0100

    removed 2 includes  in GetAsync

--- SocialWeb.Infrastructure/Repositories/UserRepository.cs
+++ SocialWeb.Infrastructure/Repositories/UserRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SocialWeb.Core.Domain;
 using SocialWeb.Core.Repositories;
 using SocialWeb.Infrastructure.EF;
+using System.Linq;
 
 namespace SocialWeb.Infrastructure.Repositories
 {
@@ -20,7 +21,9 @@ namespace SocialWeb.Infrastructure.Repositories
             => await _context.User.SingleOrDefaultAsync(x => x.Id == id);
 
         public async Task<User> GetAsync(string email)
-            => await _context.User.Include(x => x.Posts).ThenInclude(x => x.Comments).Include(x => x.Following).Include(x => x.Followers).SingleOrDefaultAsync(x => x.Email == email);
+            => await _context.User.Include(x => x.Posts).ThenInclude(x => x.Comments)
+            .Include(x => x.Following).Include(x => x.Followers)
+            .SingleOrDefaultAsync(x => x.Email == email);
 
         public async Task AddAsync(User user)
         {
